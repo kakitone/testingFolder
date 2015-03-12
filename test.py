0245e9b0a1fb00f62d8086c0527832fb3a9438d4
kakitone <kakitone@gmail.com>
2015-03-12 11:12:49 -0700

    second commit

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
+import os
 print "hi"
 assert(1==1)
+f = open("localitem.txt", 'w')
+f.write('1')
+f.close()
+
+f=open("localitem.txt", 'r')
+tmp = f.readline().rstrip()
+assert(tmp == '1')
+print "tmp: "+ tmp
+f.close()
+
+os.system("wget https://www.dropbox.com/s/rzkd0op81rj6my1/download.txt?dl=1 -O /tmp/dow")
+f=open("/tmp/dow", 'r')
+tmp = f.readline().rstrip()
+assert(tmp =='2')
+print "tmp2: "+ tmp
+f.close()
+
+
